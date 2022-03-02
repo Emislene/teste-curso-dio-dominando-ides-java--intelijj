752132da668ae0aa0d06703d049bb015fb5939a2
EMISLENE.L.L <mis_leny@hotmail.com>
2022-03-02 20:35:42 -0300

    first commit

---
+++ src/primeiroprograma.java
@@ -0,0 +1,10 @@
+import br.com.dio.model.Gato;
+
+public class primeiroprograma {
+    public static void main(String[] args) {
+            /*int a = 5;
+            int b = 3;
+            System.out.println("Hello World! " + (a + b));*/
+    }
+
+}
